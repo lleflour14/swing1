2daa4bab25eb78ac43a6fdd696764d9e71027bac
louis <louis@10.187.0.181>
2023-01-25 09:12:58 +0100

    first commit

---
+++ src/main/java/com/mycompany/projet_java/Projet_java.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_java;
+
+/**
+ *
+ * @author louis
+ */
+public class Projet_java {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
